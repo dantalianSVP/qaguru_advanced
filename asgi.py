dc1a61c677447f180250fb69aba7be8c7af0a8c8
dantalianSVP <Slender410>
2024-08-08 13:52:50 +0300

    add healthcheck

--- asgi.py
+++ asgi.py
@@ -1,7 +1,15 @@
+import json
 import uvicorn
 from main import create_app
+from serializers.user import UserBaseSchema
 
 app = create_app()
 
+users: list[UserBaseSchema]
+
 if __name__ == '__main__':
-    uvicorn.run('asgi:app', port=5000, log_level='debug')
+    with open("db/users.json") as f:
+        users = json.load(f)
+    for user in users:
+        UserBaseSchema.model_validate(user)
+    uvicorn.run('asgi:app', port=5001, log_level='debug')
